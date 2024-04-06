4dcf0ea736ed6056a513047a2ea8f754c78a1e79
zzmtsvv <zzemtsovv@yandex.ru>
2024-04-06 13:39:26 +0300

    nearly done

--- transformer.py
+++ transformer.py
@@ -1,35 +1,57 @@
 from openai import OpenAI
+from categories import (
+    BASE_USER_PROMPT,
+    BASE_CHAT_PROMPT,
+    BASE_EVENT_PROMPT,
+    OUTPUT_PROMPT
+    )
 
 
 API_KEY = ""
 
 
-class ChatBot:
+class TransformerModel:
     def __init__(self,
                  model: str = "gpt-3.5-turbo") -> None:
         self.client = OpenAI(
             api_key=API_KEY
         )
         self.model = model
-        self.base_prompt = ""
     
-    def predict(self, message_prompt: str) -> str:
+    def predict_chat(self, message_prompt: str) -> str:
         chat_completion = self.client.chat.completions.create(
             messages=[
                 {
                     "role": "user",
-                    "content": self.base_prompt + "\n" + message_prompt
+                    "content": BASE_CHAT_PROMPT
+                               + "\n"
+                               + message_prompt
                 }
             ],
             model=self.model
         )
 
         return chat_completion.choices[0].message.content
+    
+    def predict_description_to_buy(self,
+                                   user_info: str,
+                                   event_info: str) -> str:
+        chat_completion = self.client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": BASE_USER_PROMPT
+                               + "\n"
+                               + user_info
+                               + "\n"
+                               + BASE_EVENT_PROMPT
+                               + "\n"
+                               + event_info
+                               + "\n"
+                               + OUTPUT_PROMPT
+                }
+            ],
+            model=self.model
+        )
 
-
-class Recommender:
-    def __init__(self) -> None:
-        pass
-
-    def predict(self,) -> str:
-        pass
+        return chat_completion.choices[0].message.content
