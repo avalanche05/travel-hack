7c089c763d73e6a5ce5e1fe97f0776b68bc37d27
Ilin Ivan <buran05@icloud.com>
2024-04-06 23:15:29 +0300

    deploy prepare

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,3 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    # Запуск FastAPI приложения с помощью Uvicorn
-    import uvicorn
-
-    uvicorn.run(app, host="127.0.0.1", port=8000)
