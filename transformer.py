9127f641e528b594eebe681047943913ed249ac2
zzmtsvv <zzemtsovv@yandex.ru>
2024-04-06 17:48:45 +0300

    only insurance prompts

--- transformer.py
+++ transformer.py
@@ -55,3 +55,14 @@ class TransformerModel:
         )
 
         return chat_completion.choices[0].message.content
+
+
+if __name__ == "__main__":
+    model = TransformerModel()
+
+    print(model.predict_chat("привет что ты можешь посоветовать насчет страховки на билет в музей Булгакова на 14 апреля?"))
+    print()
+    print(model.predict_chat("привет можешь ли ты написать код обхода графа в ширину"))
+    print()
+    print(model.predict_description_to_buy("30 лет мужчина, два малолетних ребенка, отличные показатели здоровья",
+                                           "прогулка на свежем воздухе вдали от города по усадьбе"))
